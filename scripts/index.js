b50300269ae3c797b2a25d041185f0a8e5aecceb
Omer Ganim <ganimomer@gmail.com>
2015-07-05 15:54:25 +0300

    changed forEaches to map and reduce

--- scripts/index.js
+++ scripts/index.js
@@ -12,20 +12,17 @@ function getData() {
   }
   return Promise.all(promises)
     .then(function (pages) {
-      var events = [];
-      pages.forEach(function(eventsPage) {
-        eventsPage.forEach(function(event) {
-          events.push(event);
-        })
-      });
-      var eventTypeCount = {};
-      events.forEach(function(event) {
-        eventTypeCount[event.type] = (eventsTypeCount[event.type] || 0) + 1;
-      });
+      var events = pages.reduce(function(soFar, page) {
+        return soFar.concat(page);
+      }, []);
+      var eventTypeCount = events.reduce(function(byType, event){
+        byType[event.type] = (byType[event.type] || 0) + 1;
+        return byType;
+      }, {});
+
       var eventTypes = Object.keys(eventTypeCount);
-      var eventValues = [];
-      eventTypes.forEach(function(eventType) {
-        eventValues.push(eventTypeCount[eventType]);
+      var eventValues = eventTypes.map(function(type) {
+        return eventTypeCount[type];
       });
       return {types: eventTypes, values: eventValues}
     });
