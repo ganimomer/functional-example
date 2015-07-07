e5a763f29c1909c5de8afd9701e4b7c5a2207ac0
Omer Ganim <ganimomer@gmail.com>
2015-07-07 11:41:45 +0300

    added lodash with intermediate results

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,6 @@
 function getData() {
   var pages = window.data;
-  var events = pages.reduce(function(soFar, page) {
-    return soFar.concat(page);
-  }, []);
-
-  var eventTypeCount = events.reduce(function(soFar, event) {
-    soFar[event.type] = (soFar[event.type] || 0) + 1;
-    return soFar;
-  }, {});
-  var eventTypes = Object.keys(eventTypeCount);
-  var eventValues = eventTypes.map(function(type) {
-    return eventTypeCount[type];
-  });
-  return {types: eventTypes, values: eventValues}
+  var events = _.flatten(pages);
+  var eventTypeCount = _.countBy(events, 'type');
+  return {types: _.keys(eventTypeCount), values: _.values(eventTypeCount)};
 }
