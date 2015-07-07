98c0b54ae02842a758675b32cf4a703cfb688428
Omer Ganim <ganimomer@gmail.com>
2015-07-07 11:36:10 +0300

    changed for loops to forEach

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,19 @@
 function getData() {
   var pages = window.data;
   var events = [];
-  for (var i = 0; i < pages.length; i++) {
-    for (var j = 0; j < pages[i].length; j++) {
-      events.push(pages[i][j]);
-    }
-  }
+  pages.forEach(function(page) {
+    page.forEach(function(event) {
+      events.push(event);
+    })
+  });
   var eventTypeCount = {};
-  for (var k = 0; k < events.length; k++) {
-    if (eventTypeCount[events[k].type]) {
-      eventTypeCount[events[k].type]++
-    } else {
-      eventTypeCount[events[k].type] = 1;
-    }
-  }
+  events.forEach(function(event) {
+    eventTypeCount[event.type] = (eventTypeCount[event.type] || 0) + 1;
+  });
   var eventTypes = Object.keys(eventTypeCount);
   var eventValues = [];
-  for (var n = 0; n < eventTypes.length; n++) {
-    eventValues.push(eventTypeCount[eventTypes[n]]);
-  }
+  eventTypes.forEach(function(eventType) {
+    eventValues.push(eventTypeCount[eventType]);
+  });
   return {types: eventTypes, values: eventValues}
 }
